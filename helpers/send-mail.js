c71b0fb69052f987f547fd417f195ff2445560ba
Gilmer Contreras <adgdiux@gmail.com>
2023-05-31 09:28:45 -0500

    Correciones Send Email - Worker

--- helpers/send-mail.js
+++ helpers/send-mail.js
@@ -12,72 +12,100 @@ const sendMail = async(email, subject, html, msg) => {
 
     try {
 
-        const oauth2Client = new OAuth2(
-            accountTransport.auth.clientId,
-            accountTransport.auth.clientSecret,
-            "https://developers.google.com/oauthplayground",
-        );
-
-        oauth2Client.setCredentials({
-            refresh_token: accountTransport.auth.refreshToken,
-            tls: {
-                rejectUnauthorized: false
+        let transporter = nodemailer.createTransport({
+            host: "smtpout.secureserver.net",
+            port: 465,
+            secure: true, // use TLS
+            auth: {
+                user: "auxgestionhumana@grupofortalezasas.com",
+                pass: "Fortalezasas2022*",
             }
         });
 
-        oauth2Client.getAccessToken((err, token) => {
+        const mailOptions = {
+            from: '"Grupo Fortaleza" <nomina.fortaleza@gmail.com>', // sender address (who sends)
+            to: email, // list of receivers (who receives)
+            subject, // Subject line
+            html,
+        };
 
-            if (err) {
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, async(error, info) => {
+            if (error) {
                 console.log(error);
                 return false;
-            };
+            }
 
-            // ENVIAR EMAIL
-
-            // const transporter = nodemailer.createTransport({
-            //     'service': 'gmail',
-            //     'auth': {
-            //         'type': 'OAuth2',
-            //         'user': `${accountTransport.auth.user}`,
-            //         'clientId': `${accountTransport.auth.clientId}`,
-            //         'clientSecret': `${accountTransport.auth.clientSecret}`,
-            //         'refreshToken': `${token}`
-            //     }
-            // });
-
-            let transporter = nodemailer.createTransport({
-                host: "smtpout.secureserver.net",
-                port: 465,
-                secure: true, // use TLS
-                auth: {
-                    user: "auxgestionhumana@grupofortalezasas.com",
-                    pass: "Fortalezasas2022*",
-                },
-                tls: {
-                    // do not fail on invalid certs
-                    rejectUnauthorized: false,
-                },
-            });
-
-            const mailOptions = {
-                from: '"Grupo Fortaleza" <nomina.fortaleza@gmail.com>', // sender address (who sends)
-                to: email, // list of receivers (who receives)
-                subject, // Subject line
-                html,
-            };
-
-            // send mail with defined transport object
-            transporter.sendMail(mailOptions, async(error, info) => {
-                if (error) {
-                    console.log(error);
-                    return false;
-                }
-
-                return true;
-
-            });
+            return true;
 
         });
+
+        // const oauth2Client = new OAuth2(
+        //     accountTransport.auth.clientId,
+        //     accountTransport.auth.clientSecret,
+        //     "https://developers.google.com/oauthplayground",
+        // );
+
+        // oauth2Client.setCredentials({
+        //     refresh_token: accountTransport.auth.refreshToken,
+        //     tls: {
+        //         rejectUnauthorized: false
+        //     }
+        // });
+
+        // oauth2Client.getAccessToken((err, token) => {
+
+        //     if (err) {
+        //         console.log(err);
+        //         return false;
+        //     };
+
+        //     // ENVIAR EMAIL
+
+        //     // const transporter = nodemailer.createTransport({
+        //     //     'service': 'gmail',
+        //     //     'auth': {
+        //     //         'type': 'OAuth2',
+        //     //         'user': `${accountTransport.auth.user}`,
+        //     //         'clientId': `${accountTransport.auth.clientId}`,
+        //     //         'clientSecret': `${accountTransport.auth.clientSecret}`,
+        //     //         'refreshToken': `${token}`
+        //     //     }
+        //     // });
+
+        //     let transporter = nodemailer.createTransport({
+        //         host: "smtpout.secureserver.net",
+        //         port: 465,
+        //         secure: true, // use TLS
+        //         auth: {
+        //             user: "auxgestionhumana@grupofortalezasas.com",
+        //             pass: "Fortalezasas2022*",
+        //         },
+        //         tls: {
+        //             // do not fail on invalid certs
+        //             rejectUnauthorized: false,
+        //         },
+        //     });
+
+        //     const mailOptions = {
+        //         from: '"Grupo Fortaleza" <nomina.fortaleza@gmail.com>', // sender address (who sends)
+        //         to: email, // list of receivers (who receives)
+        //         subject, // Subject line
+        //         html,
+        //     };
+
+        //     // send mail with defined transport object
+        //     transporter.sendMail(mailOptions, async(error, info) => {
+        //         if (error) {
+        //             console.log(error);
+        //             return false;
+        //         }
+
+        //         return true;
+
+        //     });
+
+        // });
 
 
     } catch (error) {
